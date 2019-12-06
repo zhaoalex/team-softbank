39a8675eddc6333d0da7e90c109582e2f54e9d3f
Alex Zhao <alexzhao21@gmail.com>
2019-12-06 05:27:17 -0800

    demo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
               else
                 visible_posts.by_distance
               end).paginate(PAGE_LIMIT, @posts_page_num)
-    fresh_when([@posts.all, @posts.sum(&:like_count), @posts.sum { |p| p.comments_count.to_i }, @posts.map { |p| helpers.distance_of_time_in_words(p.created_at, Time.now) }, @location])
+    # fresh_when([@posts, @posts.sum(&:like_count), @posts.sum { |p| p.comments_count.to_i }, @posts.map { |p| helpers.distance_of_time_in_words(p.created_at, Time.now) }, @location])
   end
 
   def new
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     @comments_page_num_max = (@post.comments_count.to_i + (PAGE_LIMIT - 1)) / PAGE_LIMIT
     @comments_page_num = [1, [params[:comments_page_num].to_i, @comments_page_num_max].min].max
     @comments = @post.comments.order('like_count DESC').paginate(PAGE_LIMIT, @comments_page_num)
-    fresh_when([@post, @post.like_count, helpers.distance_of_time_in_words(@post.created_at, Time.now), @comments.all, @comments.sum(&:like_count), @location])
+    # fresh_when([@post, @post.like_count, helpers.distance_of_time_in_words(@post.created_at, Time.now), @comments, @comments.sum(&:like_count), @location])
   end
 
   def create
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,6 @@ class ProfilesController < ApplicationController
     @user = User.includes(posts: { image_attachment: :blob }).includes(:comments).order(:id).find(current_user.id)
     @posts = @user.posts
     @comments = @user.comments
-    fresh_when([@user.likes_count, @posts.all, @posts.sum(&:like_count), @posts.map { |p| helpers.distance_of_time_in_words(p.created_at, Time.now) }, @comments.all, @comments.sum(&:like_count), @comments.map { |c| helpers.distance_of_time_in_words(c.created_at, Time.now) }])
+    # fresh_when([@user.likes_count, @posts.all, @posts.sum(&:like_count), @posts.map { |p| helpers.distance_of_time_in_words(p.created_at, Time.now) }, @comments.all, @comments.sum(&:like_count), @comments.map { |c| helpers.distance_of_time_in_words(c.created_at, Time.now) }])
   end
 end
